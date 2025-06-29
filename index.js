7c8624267cf50f05e908c468e7e27a7aa68198e7
W-Kuria <william.njoroge.kuria@gmail.com>
2025-06-29 19:10:47 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Remove the 'main' element
+const main = document.getElementById('main');
+main.remove();
+
+// Create new header
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'William is the champion';
+document.body.append(newHeader);
